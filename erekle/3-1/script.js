575350497c81b68b318a5e529a273025e6e4982f
Erekle <erekle.sharia@gmail.com>
2024-12-24 23:52:32 +0400

    generate password

---
+++ erekle/3-1/script.js
@@ -0,0 +1,32 @@
+document.getElementById("generate-password").addEventListener("click", function () {
+    const passwordLength = 14;
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    let password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }   
+        document.getElementById("input").value = password;
+    });
+
+    const generateButton = document.getElementById("generate-password");
+    const inputField = document.getElementById("input");
+    const copyIcon = document.getElementById("fav");
+    const notification = document.getElementById("notification");
+
+
+    document.getElementById("fav").addEventListener("click", function () {
+    const passwordField = document.getElementById("input");
+    const password = passwordField.value;
+           
+        
+        const notification = document.getElementById("notification");
+        notification.style.display = "block";
+        setTimeout(() => {
+            notification.style.display = "none";
+        }, 3000);
+
+        notification.textContent = `copied!: ${password}`;
+        notification.style.display = "block";
+    });
