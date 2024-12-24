9118f3652d947d1327d8f78e3508923a83682458
unknown <dali.kvantaliani888@gmail.com>
2024-12-24 15:04:01 +0400

    first task

---
+++ dali/project 1/script.js
@@ -0,0 +1,28 @@
+function generateRandomPassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    let password = "";
+    const passwordLength = 14; 
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    } 
+    return password;
+}
+let button = document.querySelector("#button");
+button.addEventListener("click", function(){
+    document.getElementById("search").value = generateRandomPassword();
+});
+
+function showPasswordAlert(password) {
+    const alert = document.getElementById("password");
+    alert.textContent = `${password} copied!`;
+    alert.style.display = 'block';
+    setTimeout(() => {
+      alert.style.display = 'none';
+    }, 2000); 
+  }
+  document.getElementById("icon").addEventListener('click', function() {
+    const searchValue = document.getElementById("search").value;
+    const password = searchValue || generateRandomPassword();
+    showPasswordAlert(password);
+  });
