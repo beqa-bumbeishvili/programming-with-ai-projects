e70c62e2815d7a80c5fb95465379dc41213628be
kurtsika <kurtsikidze.tatia@gmail.com>
2024-12-30 18:35:47 +0400

    cleanCode

--- tatia/Calculator/script.js
+++ tatia/Calculator/script.js
@@ -15,11 +15,29 @@ function roundToDecimals(num, decimals) {
 function calculateTheResult() {
     const expression = document.getElementById('display').value;
 
-    const numbers = expression.split(/[\+\-\*\/]/).map(num => parseFloat(num));
+    let numbers = expression.split(/[\+\-\*\/]/).map(num => parseFloat(num));
     console.log(expression);
-    const operators = expression.split(/[^\+\-\*\/]/).filter(op => op !== '');
+    let operators = expression.split(/[^\+\-\*\/]/).filter(op => op !== '');
+    while (true) {
+        if (operators.includes("*")) {
+            let multiplyIndex = operators.indexOf("*");
+            numbers[multiplyIndex] = numbers[multiplyIndex] * numbers[multiplyIndex + 1];
+            numbers.splice(multiplyIndex + 1, 1);
+            operators.splice(multiplyIndex, 1);
 
+        } else if ((operators.includes("/"))) {
+            let divideIndex = operators.indexOf("/");
+            numbers[divideIndex] = numbers[divideIndex] / numbers[divideIndex + 1];
+            numbers.splice(divideIndex + 1, 1);
+            operators.splice(divideIndex, 1);
+
+        } else {
+            break;
+        }
+    }
     let result = numbers[0];
+
+
 
     for (let i = 0; i < operators.length; i++) {
         switch (operators[i]) {
