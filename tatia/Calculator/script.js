aae5875e6a5e32bcb10c50f3b2d353d0c0add936
kurtsika <kurtsikidze.tatia@gmail.com>
2024-12-30 13:34:26 +0400

    CalculatorCleanCode

--- tatia/Calculator/script.js
+++ tatia/Calculator/script.js
@@ -1,16 +1,48 @@
-function appendToDisplay(value) {
-  document.getElementById('display').value += value;
+
+function appendValuesToDisplay(value) {
+    document.getElementById('display').value += value;
 }
 
 function clearDisplay() {
-  document.getElementById('display').value = '';
+    document.getElementById('display').value = '';
 }
 
-function calculate() {
-  try {
-    const result = eval(document.getElementById('display').value);
+function roundToDecimals(num, decimals) {
+    let factor = Math.pow(10, decimals);
+    return Math.round(num * factor) / factor;
+}
+
+function calculateTheResult() {
+    const expression = document.getElementById('display').value;
+
+    const numbers = expression.split(/[\+\-\*\/]/).map(num => parseFloat(num));
+    console.log(expression);
+    const operators = expression.split(/[^\+\-\*\/]/).filter(op => op !== '');
+
+    let result = numbers[0];
+
+    for (let i = 0; i < operators.length; i++) {
+        switch (operators[i]) {
+            case '+':
+                result += numbers[i + 1];
+                break;
+            case '-':
+                result -= numbers[i + 1];
+                break;
+            case '*':
+                result *= numbers[i + 1];
+                break;
+            case '/':
+                if (numbers[i + 1] !== 0) {
+                    result /= numbers[i + 1];
+                } else {
+                    document.getElementById('display').value = 'Error';
+                    return;
+                }
+                break;
+        }
+    }
+    result = roundToDecimals(result, 8)
+
     document.getElementById('display').value = result;
-  } catch (e) {
-    document.getElementById('display').value = 'Error';
-  }
 }
