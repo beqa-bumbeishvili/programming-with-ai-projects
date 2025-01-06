9bb0bb5c9bac53cbc34af3201f6b477ed3e1e2c1
Tamuna <tamuna.kvantaliani4@gmail.com>
2025-01-06 23:21:40 +0400

    areaChart

--- tamuna/areaChart/areaChart.js
+++ tamuna/areaChart/areaChart.js
@@ -2,19 +2,143 @@ class AreaChart {
     constructor() {
         const attrs = {
             id: "ID" + Math.floor(Math.random() * 1000000),
-            svgWidth: 800,
-            svgHeight: 400,
-            marginTop: 10,
-            marginBottom: 30,
-            marginRight: 30,
-            marginLeft: 50,
             container: "body",
+            svgWidth: 1000,
+            svgHeight: 400,
+            width: null,
+            height: null,
+            marginTop: 40,
+            marginBottom: 40,
+            marginRight: 20,
+            marginLeft: 40,
             defaultTextFill: "#2C3E50",
             defaultFont: "Helvetica",
-            data: null,
-            chartWidth: null,
-            chartHeight: null
+            mainContainerStyles: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "400px",
+                marginTop: "80px",
+            },
+            contentContainerStyles: {
+                width: "800px",
+                maxWidth: "700px"
+            },
+            titleGroupStyles: {
+                marginBottom: "30px"
+            },
+            companyTitleStyles: {
+                fontSize: "18px",
+                fontFamily: "Helvetica",
+                marginLeft: "5px",
+                opacity: "0.8"
+            },
+            priceGroupStyles: {
+                position: "relative",
+                marginLeft: "5px"
+            },
+            mainPriceStyles: {
+                fontSize: "36px",
+                fontFamily: "Helvetica",
+                fontWeight: "500",
+                margin: "0",
+                marginTop: "10px"
+            },
+            priceDeltaStyles: {
+                position: "absolute",
+                left: "0",
+                top: "30px",
+                fontSize: "14px",
+                fontFamily: "Arial, sans-serif",
+                color: "#39A93B",
+                margin: "0",
+                marginTop: "20px"
+            },
+            preMarketStyles: {
+                position: "absolute",
+                left: "2px",
+                top: "10px",
+                fontSize: "14px",
+                fontFamily: "Arial, sans-serif",
+                color: "black",
+                fontWeight: "400",
+                marginTop: "75px",
+                marginBottom: "60px"
+            },
+            xAxisTextStyles: {
+                fontSize: "14px",
+                fill: "grey",
+                fontFamily: "Arial, sans-serif",
+                textAnchor: "middle"
+            },
+            yAxisTextStyles: {
+                fontSize: "14px",
+                fill: "grey",
+                fontFamily: "Arial, sans-serif",
+                textAnchor: "end"
+            },
+            gridStyles: {
+                stroke: "#e0e0e0",
+                opacity: "0.5"
+            },
+            areaStyles: {
+                fillGradientTop: {
+                    color: "#39A93B",
+                    opacity: 0.4
+                },
+                fillGradientBottom: {
+                    color: "#39A93B",
+                    opacity: 0
+                }
+            },
+            lineStyles: {
+                stroke: "#39A93B",
+                strokeWidth: 2
+            },
+            
+            grid: {
+                horizontal: {
+                    stroke: "lightgrey",
+                    opacity: 0.1,
+                    strokeWidth: 1,
+                    config: {
+                        ticks: 5,
+                        tickSize: null  
+                    }
+                },
+                vertical: {
+                    stroke: "lightgrey",
+                    opacity: 0.1,
+                    strokeWidth: 1,
+                    config: {
+                        ticks: 5,
+                        tickSize: null
+                    }
+                }
+            },
+            xAxis: {
+                text: {
+                    fontSize: "14px",
+                    fill: "grey",
+                    fontFamily: "Arial, sans-serif",
+                    textAnchor: "middle",
+                    dx: "15",
+                    dy: "25"
+                },
+                line: {
+                    stroke: "none"
+                },
+                ticks: {
+                    number: 5
+                }
+            }
         };
+
+        attrs.width = attrs.svgWidth - attrs.marginLeft - attrs.marginRight;
+        attrs.height = attrs.svgHeight - attrs.marginTop - attrs.marginBottom;
+        attrs.data = [];  
+
+        this.attrs = attrs;
 
         this.getState = () => attrs;
         this.setState = (d) => Object.assign(attrs, d);
@@ -30,6 +154,7 @@ class AreaChart {
 
     render() {
         const attrs = this.getState();
+        console.log('Rendering with data:', attrs.data);
         const margin = { 
             top: attrs.marginTop, 
             right: attrs.marginRight, 
@@ -50,30 +175,36 @@ class AreaChart {
     }
 
     createContainers(width, height, margin) {
-        const mainContainer = d3.select(this.container())
+        const attrs = this.getState();
+
+        d3.select(attrs.container).selectAll("*").remove();
+
+        const mainContainer = d3.select(attrs.container)
             .append("div")
-            .style("width", "100%")
-            .style("max-width", "800px")
-            .style("margin", "0 auto")
-            .style("padding-top", "40px");
+            .style("display",attrs.mainContainerStyles.display)
+            .style("justify-content",attrs.mainContainerStyles.justifyContent)
+            .style("align-items",attrs.mainContainerStyles.alignItems)
+            .style("width", attrs.mainContainerStyles.width)
+            .style("margin", attrs.mainContainerStyles.margin)
+            .style("margin-top",attrs.mainContainerStyles.marginTop);
 
         const contentContainer = mainContainer.append("div")
-            .style("width", "100%");
+            .style("width", attrs.contentContainerStyles.width);
 
         const titleGroup = contentContainer
             .insert("div", "svg")
-            .style("margin-bottom", "100px");
+            .style("margin-bottom", attrs.titleGroupStyles.marginBottom);
 
         titleGroup.append("div")
-            .style("font-size", "18px")
-            .style("font-family", "Helvetica")
-            .style("margin-left", "10px")
-            .style("opacity", "0.8")
+            .style("font-size", attrs.companyTitleStyles.fontSize)
+            .style("font-family", attrs.companyTitleStyles.fontFamily)
+            .style("margin-left", attrs.companyTitleStyles.marginLeft)
+            .style("opacity", attrs.companyTitleStyles.opacity)
             .text("Apple Inc (AAPL)");
 
         const priceGroup = titleGroup.append("div")
-            .style("position", "relative")
-            .style("margin-left", "10px");
+            .style("position", attrs.priceGroupStyles.position)
+            .style("margin-left", attrs.priceGroupStyles.marginLeft);
 
         const svg = contentContainer.append("svg")
             .attr("width", "100%")
@@ -101,9 +232,9 @@ class AreaChart {
     }
 
     drawAxes(width, height) {
-        const { svg, x, y } = this.getState();
+        const attrs = this.getState();
+        const { svg, x, y } = attrs;
 
-        // X axis
         const xAxis = d3.axisBottom(x)
             .ticks(5)
             .tickSize(0)
@@ -116,16 +247,15 @@ class AreaChart {
             .attr("transform", `translate(0,${height})`)
             .call(xAxis)
             .selectAll("text")
-            .style("font-size", "14px")
-            .style("fill", "grey")
-            .style("font-family", "Arial, sans-serif")
-            .style("text-anchor", "middle")
-            .attr("dx", "15")
-            .attr("dy", "25");
+            .style("font-size", attrs.xAxis.text.fontSize)
+            .style("fill", attrs.xAxis.text.fill)
+            .style("font-family", attrs.xAxis.text.fontFamily)
+            .style("text-anchor", attrs.xAxis.text.textAnchor)
+            .attr("dx", attrs.xAxis.text.dx)
+            .attr("dy", attrs.xAxis.text.dy);
 
         svg.select("g").select("path").remove();
 
-        // Y axis
         const yAxis = d3.axisLeft(y)
             .tickValues([246, 248, 250, 252, 254, 256])
             .tickSize(0);
@@ -134,63 +264,72 @@ class AreaChart {
             .call(yAxis)
             .attr("class", "y-axis")
             .selectAll("text")
-            .style("font-size", "14px")
-            .style("fill", "grey")
-            .style("font-family", "Arial, sans-serif")
-            .style("text-anchor", "end")
-            .attr("dx", "-10");
+            .style("font-size", attrs.yAxisTextStyles.fontSize)
+            .style("fill", attrs.yAxisTextStyles.fill)
+            .style("font-family", attrs.yAxisTextStyles.fontFamily)
+            .style("text-anchor", attrs.yAxisTextStyles.textAnchor)
+            .attr("dx", "-8");
 
         d3.select(".y-axis").selectAll("path, line").remove();
     }
 
     drawChart(width, height) {
-        const { svg, x, y, data } = this.getState();
+        const attrs = this.getState();
+        const { svg, data, x, y } = attrs;
 
-        // Add gridlines
-        this.drawGridlines(width);
+        if (!attrs.area) {
+            attrs.area = {
+                gradient: {
+                    top: { color: "#39A93B", opacity: 0.4 },
+                    bottom: { color: "#39A93B", opacity: 0 }
+                },
+                path: {}
+            };
+        }
 
-        // Create gradient
-        const areaGradient = svg.append("defs")
+        const gradientId = `area-gradient-${attrs.id}`;
+        const gradient = svg.append("defs")
             .append("linearGradient")
-            .attr("id", "areaGradient")
+            .attr("id", gradientId)
             .attr("x1", "0%")
-            .attr("x2", "0%")
             .attr("y1", "0%")
+            .attr("x2", "0%")
             .attr("y2", "100%");
 
-        areaGradient.append("stop")
+        gradient.append("stop")
             .attr("offset", "0%")
-            .attr("stop-color", "#39A93B")
-            .attr("stop-opacity", 0.4);
+            .attr("stop-color", attrs.area.gradient.top.color)
+            .attr("stop-opacity", attrs.area.gradient.top.opacity);
 
-        areaGradient.append("stop")
+        gradient.append("stop")
             .attr("offset", "100%")
-            .attr("stop-color", "#39A93B")
-            .attr("stop-opacity", 0);
+            .attr("stop-color", attrs.area.gradient.bottom.color)
+            .attr("stop-opacity", attrs.area.gradient.bottom.opacity);
 
-        // Add area
         const area = d3.area()
             .x(d => x(d.date))
             .y0(height)
-            .y1(d => y(d.value))
-            .curve(d3.curveLinear);
+            .y1(d => y(d.price));
 
         svg.append("path")
             .datum(data)
-            .attr("fill", "url(#areaGradient)")
-            .attr("d", area);
+            .attr("class", "area")
+            .attr("d", area)
+            .style("fill", `url(#${gradientId})`);
 
-        // Add line
+        const line = d3.line()
+            .x(d => x(d.date))
+            .y(d => y(d.price));
+
         svg.append("path")
             .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "#39A93B")
-            .attr("stroke-width", 2)
-            .attr("d", d3.line()
-                .x(d => x(d.date))
-                .y(d => y(d.value))
-                .curve(d3.curveLinear)
-            );
+            .attr("class", "line")
+            .attr("d", line)
+            .style("fill", "none")
+            .style("stroke", "#39A93B")
+            .style("stroke-width", "2px");
+
+        this.drawGridlines(width);
     }
 
     drawGridlines(width) {
@@ -215,58 +354,52 @@ class AreaChart {
     }
 
     updatePriceInfo() {
-        const { priceGroup } = this.getState();
+        const attrs = this.getState();
+        const { priceGroup } = attrs;
 
         priceGroup.append("h1")
-            .style("font-size", "36px")
-            .style("font-family", "Helvetica")
-            .style("font-weight", "500")
-            .style("margin", "0")
-            .style("margin-top", "10px")
+            .style("font-size", attrs.mainPriceStyles.fontSize)
+            .style("font-family", attrs.mainPriceStyles.fontFamily)
+            .style("font-weight", attrs.mainPriceStyles.fontWeight)
+            .style("margin", attrs.mainPriceStyles.margin)
+            .style("margin-top", attrs.mainPriceStyles.marginTop)
             .text("$249.79");
 
         priceGroup.append("h2")
-            .style("position", "absolute")
-            .style("left", "0")
-            .style("top", "40px")
-            .style("font-size", "14px")
-            .style("font-family", "Arial, sans-serif")
-            .style("color", "#39A93B")
-            .style("margin", "0")
-            .style("margin-top", "20px")
-            .html("+$2.14 (0.86%) <span style='color: black; font-family: Helvetica;\
-               font-weight: 500; margin-left: 10px'> Past 5 days</span>");
+            .style("font-size", attrs.priceDeltaStyles.fontSize)
+            .style("font-family", attrs.priceDeltaStyles.fontFamily)
+            .style("color", attrs.priceDeltaStyles.color)
+            .style("margin", attrs.priceDeltaStyles.margin)
+            .style("margin-top", attrs.priceDeltaStyles.marginTop)
+            .html("+$2.14 (0.86%) <span style='color: black;\
+                font-family: Helvetica; font-weight: 300; margin-left: 10px'> Past 5 days</span>");
 
         priceGroup.append("h3")
-            .style("position", "absolute")
-            .style("left", "2px")
-            .style("top", "40px")
-            .style("font-size", "14px")
-            .style("font-family", "Arial, sans-serif")
-            .style("color", "black")
-            .style("font-weight", "550")
-            .style("margin", "0")
-            .style("margin-top", "45px")
-            .style("margin-bottom", "40px")
+            .style("position", attrs.preMarketStyles.position)
+            .style("left", attrs.preMarketStyles.left)
+            .style("top", attrs.preMarketStyles.top)
+            .style("font-size", attrs.preMarketStyles.fontSize)
+            .style("font-family", attrs.preMarketStyles.fontFamily)
+            .style("color", attrs.preMarketStyles.color)
+            .style("font-weight", attrs.preMarketStyles.fontWeight)
+            .style("margin-top", attrs.preMarketStyles.marginTop)
             .html(`$247.32 <span style='color: red; font-family: Helvetica;\
-               font-weight:350; margin-left: 10px'> -$2.47 (-0.99%)</span><span style='color: black; font-family: Helvetica;\
-               font-weight:350;margin-left: 10px'> Pre-Market</span>`);
+                 font-weight:350; margin-left: 10px'> -$2.47 (-0.99%)</span><span \
+                 style='color: black; font-family: Helvetica; font-weight:300;margin-left: 10px'> Pre-Market</span>`);
     }
 
     setDynamicContainer() {
         const attrs = this.getState();
         
-        // Get container width
         const containerEl = d3.select(attrs.container).node();
         const containerWidth = containerEl.getBoundingClientRect().width;
         
-        // Update dimensions
-        attrs.svgWidth = Math.min(containerWidth, 800);
-        attrs.svgHeight = Math.max(attrs.svgWidth * 0.5, 300); // minimum height of 300px
-        attrs.chartWidth = attrs.svgWidth - attrs.marginLeft - attrs.marginRight;
-        attrs.chartHeight = attrs.svgHeight - attrs.marginTop - attrs.marginBottom;
+        attrs.svgWidth = Math.min(containerWidth * 0.95, 600); 
+        attrs.svgHeight = Math.min(attrs.svgWidth * 0.5, 300); 
+        
+        attrs.width = attrs.svgWidth - attrs.marginLeft - attrs.marginRight;
+        attrs.height = attrs.svgHeight - attrs.marginTop - attrs.marginBottom;
 
-        // Add resize listener (with cleanup of old listener)
         const resizeHandler = () => {
             const newWidth = containerEl.getBoundingClientRect().width;
             attrs.svgWidth = Math.min(newWidth, 800);
@@ -278,35 +411,21 @@ class AreaChart {
             this.render();
         };
 
-        // Remove old listener if exists
         if (window._chartResizeHandler) {
             window.removeEventListener('resize', window._chartResizeHandler);
         }
         
-        // Add new listener
         window._chartResizeHandler = resizeHandler;
         window.addEventListener('resize', resizeHandler);
 
         return this;
     }
-}
 
-// Data loading
-d3.csv("areaChart.csv", d => {
-    const parseDate = d3.timeParse("%b %d %H:%M");
-    const date = parseDate(d.date);
-    
-    return {
-        date: date,
-        value: +d.value
-    };
-}).then(data => {
-    console.log("Data loaded:", data);
-    const chart = new AreaChart()
-        .container("#chart")
-        .data(data)
-        .setDynamicContainer()
-        .render();
-}).catch(error => {
-    console.error("Error loading data:", error);
-});
+    getState() {
+        return this.attrs;  
+    }
+
+    setState(d) {
+        Object.assign(this.attrs, d);
+    }
+}
