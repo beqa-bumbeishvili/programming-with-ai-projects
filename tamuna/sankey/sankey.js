0feb55860224155341d3898d682a2779768db2a4
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2025-01-11 22:44:11 +0400

    add tamuna comments

--- tamuna/sankey/sankey.js
+++ tamuna/sankey/sankey.js
@@ -60,13 +60,13 @@ class Chart {
         };
 
         const sankey = d3.sankey()
-            .nodeWidth(10)
+            .nodeWidth(10) //attrs-ში ესენი
             .nodePadding(12)
             .extent([[1, 50], [width+400, height-200]])
             .nodeAlign(d3.sankeyLeft)
             .nodeSort((a, b) => {
-                if (a.name === "Income") return -1;
-                if (b.name === "Income") return 1;
+                if (a.name === "Income") return -1; //არაა აუცილებელი სურათის მიხედვით იყოს დასორტირებული, სანკეი სულ უნდა მუშაობდეს რა დატაც არ უნდა შემოვიცეს csv ფაილიდან
+                if (b.name === "Income") return 1;  // შეილება Income და Savings სულ არ იყოს იმ დატაში, აქ შეგიძლია დაასორტირო ალფავიტის მიხედვით მაგალითად
                 if (a.name === "Savings") return 2;
                 if (b.name === "Savings") return -2;
                 
@@ -95,6 +95,7 @@ class Chart {
         const colorScale = d3.scaleOrdinal()
             .domain(['Income', 'Savings', 'Food', 'Housing', 'Shopping', 'Taxes', 'Health', 'Other', 'Transportation', 'Entertainment'])
             .range(['#A4BFD0', '#A4BFD0', '#F6BB9F', '#D69B7F', '#D69B7F', '#9BC4A5', '#D69B7F', '#9BC4A5', '#D69B7F', '#9BC4A5']);
+            // ატტრს-ში
 
         const link = chart.append("g")
             .selectAll("path")
@@ -103,7 +104,7 @@ class Chart {
             .attr("class", "link")
             .attr("d", d3.sankeyLinkHorizontal())
             .attr("d", d => {
-                if (d.target.name === "Housing" || d.target.name === "Entertainment") {
+                if (d.target.name === "Housing" || d.target.name === "Entertainment") { //ნწუ, დატაში შეილება ვაფშე არ გქონდეს ეგენი
                     const source = d.source;
                     const target = d.target;
                     const x0 = source.x0;
@@ -115,7 +116,7 @@ class Chart {
                             C${(x0 + x1) / 2},${y0}
                              ${(x0 + x1) / 2},${y1}
                              ${x1},${y1}`;
-                }
+                } //ხაზი გამოტოვე ქვევით
                 return d3.sankeyLinkHorizontal()(d);
             })
             .attr("stroke", d => colorScale(d.target.name))
@@ -126,7 +127,7 @@ class Chart {
                 tag: 'div',
                 className: 'tooltip'
             })
-            
+  // ორი ხაზი ზედმეტია
 
         const node = chart.append("g")
             .selectAll("rect")
@@ -136,7 +137,7 @@ class Chart {
             .attr("x", d => d.x0)
             .attr("y", d => {
 
-                if (d.name === "Housing") {
+                if (d.name === "Housing") { //ნწუ, დატაში შეილება ვაფშე არ გქონდეს ეს
                     return d.y0 - 50;  
                 }
                 return d.y0;
@@ -160,6 +161,7 @@ class Chart {
             let xOffset = 40;  
             let yOffset = -15; 
             
+            // ნწუ, დატაში შეილება ვაფშე არ გქონდეს ესენი. hardCode ქვია ამას, ხელით რო ასწორებ კონკრეტულ რაღაცებს, არ მოსულა ;დ
             if (d.name === "Income") {
                 xOffset = 90; 
              yOffset = -30; 
